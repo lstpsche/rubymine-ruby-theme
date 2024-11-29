52e81f0e4e7f376aa1fc780942279193ea524cb7
GitHub Action <action@github.com>
2024-11-29 13:08:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/lstpsche/rubyminerubytheme/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/lstpsche/rubyminerubytheme/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.lstpsche.rubyminerubytheme.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
